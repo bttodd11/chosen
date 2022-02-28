1850faa4f1e5a2191eccc2c390fc03d6e7f4dbc9
Brian <BTodd@Brians-MacBook-Pro.local>
2022-02-27 21:49:43 -0600

    Updated BG image and also added PNG

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import youTube from './img/youtube.png';
+import instagram from './img/instagram.png'
+import twitter from './img/twitter.png'
+import facebook from './img/facebook.png'
 
 function App() {
   return (
     <div className="backgroundImage">
-      Test
+      <h3 className="musicTitle">Music</h3>
+      <h3 className="artTitle">Art</h3>
+      <p className="clothTitle">Cloth</p>
+      <div className="socialSection">
+      <a href=""><img title="navIcon" src={youTube} alt="my-img" class="socialIcon" /></a>
+      <a href=""><img title="navIcon" src={instagram} alt="my-img" class="socialIcon" /></a>
+      <a href=""><img title="navIcon" src={twitter} alt="my-img" class="socialIcon" /></a>
+      <a href=""><img title="navIcon" src={facebook} alt="my-img" class="socialIcon" /></a>
+      </div>
     </div>
   );
 }
