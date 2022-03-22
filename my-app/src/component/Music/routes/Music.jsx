0499726c42a99d7a5d8bf49a95b7fc14ad982047
Brian <BTodd@Brians-MacBook-Pro.local>
2022-03-21 21:00:52 -0500

    Adding GH pages

--- my-app/src/component/Music/routes/Music.jsx
+++ my-app/src/component/Music/routes/Music.jsx
@@ -6,49 +6,21 @@ import playVideo from '../../../img/play.png';
 
 const Music = () => {
 
-    let [videos, setVideos] = useState([]);
-    let playlistId = "UUwwrP7TPQmlkFHCWdCcYNpw";
-
-    const youtubeFetch = async (call) => {
-
-        const http = new XMLHttpRequest();
-        const url = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId=" + playlistId + "&key=AIzaSyBd26sX_fII61osu5b29kaOlB5odp9uMW4"
-
-        http.open("GET", url);
-        http.send();
-
-        http.onload = function () {
-            call(http.responseText)
-        };
-    }
-
-    function mycallback(data) {
-        setVideos(JSON.parse(data).items)
-        console.log(videos)
-    }
-
-    useEffect(() => {
-        youtubeFetch(mycallback)
-    }, [videos.length])
 
     return (
         <div id="musicSection">
             <h1>Music</h1>
             <Container>
                 <Row>
-                    {videos.length !== 0 ? (
-                        videos.map((vid) => (
-                            <Col md="4">
-                                <div id="videoSection">
-                                    <h5 className="videoTitle">{vid.snippet.title}</h5>
-                                    <div className="imageDiv">
-                                        <img className="playVideo" src={playVideo} />
-                                    {vid.snippet.thumbnails.maxres ? <img className="thumbnail" src={vid.snippet.thumbnails.maxres.url} /> : <img className="thumbnail" src={vid.snippet.thumbnails.default.url} />}
-                                    </div>
-                                </div>
-                            </Col>
-                        ))
-                    ) : null}
+                    <Col md="4" className="videoSection">
+                    <iframe width="300" height="300" src="https://www.youtube.com/embed/UbfSjp4Sg4A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=false; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </Col>
+                    <Col md="4" className="videoSection">
+                    <iframe width="300" height="300" src="https://www.youtube.com/embed/iN0-W_N4a24" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=false; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </Col>
+                    <Col md="4" className="videoSection">
+                    <iframe width="300" height="300" src="https://www.youtube.com/embed/z1fD3CginDk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=false; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </Col>
                 </Row>
             </Container>
 
