457b694013e646e88f10ceb6259ece20523d0910
Brian <BTodd@Brians-MacBook-Pro.local>
2022-03-22 22:34:20 -0500

    Updates

---
+++ precache-manifest.37db52a0fb950b99ea4fc14501f67509.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98df739522341d3e463245b294694a53",
+    "url": "/chosen/index.html"
+  },
+  {
+    "revision": "60c7aba6f010ab087181",
+    "url": "/chosen/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "faa9626884cfa3585f0f",
+    "url": "/chosen/static/css/main.198ce1fe.chunk.css"
+  },
+  {
+    "revision": "60c7aba6f010ab087181",
+    "url": "/chosen/static/js/2.ef7ebe08.chunk.js"
+  },
+  {
+    "revision": "faa9626884cfa3585f0f",
+    "url": "/chosen/static/js/main.59527b77.chunk.js"
+  },
+  {
+    "revision": "907f4d7725f19c8969e5",
+    "url": "/chosen/static/js/runtime-main.8f650f92.js"
+  },
+  {
+    "revision": "e0bbbe83ade90cc75b615f4445ae8565",
+    "url": "/chosen/static/media/backgroundImage.e0bbbe83.jpg"
+  },
+  {
+    "revision": "d8223b2ef58568de901b8bddc68ace3f",
+    "url": "/chosen/static/media/instagram.d8223b2e.png"
+  },
+  {
+    "revision": "74f9dc72402b25f4a27901c36140e2c1",
+    "url": "/chosen/static/media/play.74f9dc72.png"
+  }
+]);
