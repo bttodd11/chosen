d0022ca806373b19c5768ecd4b6e173644d52c7c
Brian <BTodd@Brians-MacBook-Pro.local>
2022-03-23 21:08:23 -0500

    Deleted dupl folder

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Music from './component/Music/routes/Music.jsx';
 import './index.css';
 import App from './App';
@@ -8,12 +8,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
     <Route path="/" element={<App />} />
     <Route path="music" element={<Music />} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
