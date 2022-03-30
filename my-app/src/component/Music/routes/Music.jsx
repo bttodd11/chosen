9a3fb4695020c756b3a2c121a97033a305e94c35
Brian <BTodd@Brians-MacBook-Pro.local>
2022-03-30 08:26:20 -0500

    Removed duplicated files

--- my-app/src/component/Music/routes/Music.jsx
+++ my-app/src/component/Music/routes/Music.jsx
@@ -1,29 +1,47 @@
-import "./Music.css";
-import React from "react";
-import { useEffect, useState } from "react";
+import {React, useState, useEffect} from "react";
 import { Container, Col, Row } from 'react-bootstrap';
-import playVideo from '../../../img/play.png';
+import PreLoader from '../img/loader.png';
+import "./Music.css";
+import $ from 'jquery';
 
 const Music = () => {
+    const [preLoader, removePreLoader] = useState(true);
+
+
+    useEffect(() => {
+
+
+        document.querySelector("#youtubeVideo", "#youtubeVideo1", "#youtubeVide2").addEventListener( "load", function(e) {
+            console.log("Loaded")
+            $('#preLoaderSection').hide()
+            $('#musicSection').show(); 
+        } );
+        
+    })   
 
 
     return (
-        <div id="musicSection">
-            <h1>Music</h1>
-            <Container>
-                <Row>
-                    <Col md="4" className="videoSection">
-                    <iframe width="300" height="300" src="https://www.youtube.com/embed/UbfSjp4Sg4A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=false; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </Col>
-                    <Col md="4" className="videoSection">
-                    <iframe width="300" height="300" src="https://www.youtube.com/embed/iN0-W_N4a24" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=false; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </Col>
-                    <Col md="4" className="videoSection">
-                    <iframe width="300" height="300" src="https://www.youtube.com/embed/z1fD3CginDk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=false; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </Col>
-                </Row>
-            </Container>
-
+        
+        <div>
+            <div id="preLoaderSection">
+                <img className="preLoaderImage" src={PreLoader}></img>
+            </div>
+            <div id="musicSection">
+                <h1>Music</h1>
+                <Container>
+                    <Row>
+                        <Col md="4" id="videoSection">
+                            <iframe id="youtubeVideo" allowFullScreen="true" width="200" height="200" src="https://www.youtube.com/embed/UbfSjp4Sg4A" title="YouTube video player" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                        </Col>
+                        <Col md="4" id="videoSection">
+                            <iframe id="youtubeVideo1" allowFullScreen="true" width="200" height="200" src="https://www.youtube.com/embed/iN0-W_N4a24" title="YouTube video player" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                        </Col>
+                        <Col md="4" id="videoSection">
+                            <iframe id="youtubeVideo2" allowFullScreen="true" width="200" height="200" src="https://www.youtube.com/embed/z1fD3CginDk" title="YouTube video player" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     )
 }
